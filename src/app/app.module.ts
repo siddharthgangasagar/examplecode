c02cb04a3513037193a6a4e8c0c246afc1296a67
siddharthgangasagar <74105056+siddharthgangasagar@users.noreply.github.com>
2025-07-18 16:54:42 +0530

    ..

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,5 +6,7 @@ import { RouterModule } from '@angular/router';
   standalone: true,
   imports: [RouterModule],
   template: `<router-outlet></router-outlet>`
+
+  
 })
 export class AppComponent {}
